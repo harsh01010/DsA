3f85b059652a4d7f51c6171162b5b252448761b7
Atul <atulharsh274@gmail.com>
2022-08-27 20:16:43 +0530

    commited

--- t6_sorting/mergeSort.cpp
+++ t6_sorting/mergeSort.cpp
@@ -1,21 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 
- /* merge sort
- -> divide and conqur algorithm
- -> stable algorithm
- -> nlogn , n space
- -> well suited for linked lists
- -> used in external sorting
- -> quick sort is better than merger sort.
- */
+void mergeArr(int arr[], int l, int h, int m)
+{
+    int aux1[m - l + 1], aux2[h - m];
+    int p = 0, k = 0, q = 0;
+    for (int i = 0; i <= m; i++)
+        aux1[p++] = arr[k++];
+    for (int i = m + 1; i < h; i++)
+        aux2[q++] = arr[k++];
+    q = 0, p = 0, k = 0;
+    while (p < m - l + 1 and q < h - m)
+    {
+        if (aux1[p] < aux2[q])
+            arr[k++] = aux1[p++];
+        else
+            arr[k++] = aux2[q++];
+    }
+    while (p < m - l + 1)
+
+        arr[k++] = aux1[p++];
+
+    while (q < h - m)
+
+        arr[k++] = aux2[q++];
+}
+
+void mergeSort(int  arr[],int l,int h)
+{
+      
+}
+
 int main()
 {
 
-     
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n;)
+        cin >> arr[i++];
+    int l = 0, h = n - 1;
+    mergeSort(arr, l, h);
+    for (auto it : arr)
+        cout << it << " ";
+    cout << "\n";
 
-
-
-return 0;
+    return 0;
 }
