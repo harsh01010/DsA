4dc30d7eb09494caa0c769aef008cd6ba2f47b57
Atul <atulharsh274@gmail.com>
2022-09-02 12:59:45 +0530

    added recursive bubble sort

--- t6_sorting/bubbleSort.cpp
+++ t6_sorting/bubbleSort.cpp
@@ -46,3 +46,35 @@ int main()
 
 return 0;
 }
+
+
+/*  RECURSIVE BuBBle sort
+  #include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ void rec(int arr[],int i,int j=0)
+ {
+     if(i==1) return;
+      else if(j<i-1)
+      {  
+             if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+             rec(arr,i,j+1);
+      }
+      else{
+             rec(arr,i-1);
+      }
+ }
+int main()
+{
+      int n; cin>>n;
+      int arr[n];
+      for(auto &it:arr) cin>>it;
+       
+       rec(arr,n);
+
+       for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+return 0;
+}
+
+*/
