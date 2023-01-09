d9f81923d4efd8cc3fd18d0ae68038f677f3f50d
Atul <atulharsh274@gmail.com>
2023-01-09 22:16:12 +0530

    commit

--- t16_tree/z.cpp
+++ t16_tree/z.cpp
@@ -13,82 +13,68 @@ struct node
             left = right = NULL;
       }
 };
-int height(node *root)
+
+// the below function will create a path vector from node to root
+bool pathFind(node *root, int n, vector<node *> &vec)
 {
       if (root == NULL)
-            return 0;
-      return max(height(root->left), height(root->right)) + 1;
+            return false;
+      vec.push_back(root);
+      if (root->data == n)
+            return true;
+      if (pathFind(root->left, n, vec) || pathFind(root->right, n, vec))
+            return true; // if there exist a path from curr node to desired node then return  true.
+
+      // else
+      vec.pop_back(); // we have pushed a wrong node , pop it.
+      return false;
 }
 
-// the diameter of binary tree will be max of (left height + right height +1) of all the nodes.
-
-/*
-method 1:
-calculate (left height + right height +1) for each node and find the maximum value
-this approach has time complexity of O(n^2)
+node *LCA(node *root, int n1, int n2)
+{
+      vector<node *> path1, path2;
+      if (!pathFind(root, n1, path1))
+            return NULL;
+      if (!pathFind(root, n2, path2))
+            return NULL;
+      // iterate the path arrays for both nodes , untill we find first mismatch, all previous elemetns will be ancestor of these two nodes,node before the mismatch will be lowest comman ancestor for two nodes.
+      int i = 0;
+      for (; i < path1.size() - 1 && i < path2.size() - 1; i++)
+      {
+            if (path1[i + 1] != path2[i + 1])
+                  return path1[i];
+      }
+      /*
+      10 , 30 ,40
+      10 , 30 ,40 ,60
+      in above we will return 40
 */
 
-int diameter(node *root)
-{
-      if (root == NULL)
-            return 0;
-      int d = height(root->left) + height(root->right) + 1;
-      int l = diameter(root->left);
-      int r = diameter(root->right);
-      return max(d, max(l, r));
+      if (i < path1.size() - 1)
+      {
+            return path2[i];
+      }
+      if (i < path2.size() - 1)
+      {
+            return path1[i];
+      }
+
+      return NULL;
 }
 
 /*
-method 2:
-use above method , but insted of calculating the height each time , just precompute it.
-use unordered_map<node*,int>
-*/
+for above solution.
 
-unordered_map<node *, int> ht;
+the  time complexity of find path function is theeta n (its a simple traversal of the tree) (theeta n + theeta n)
+Lca function is O(n) time.
+overall Tc => theeta (n)
+aux space => O(n)
 
-void precompute(node *root)
-{
-      if (root == NULL)
-      { ht[root] = 0; return;}
-      ht[root] = height(root);
-      precompute(root->left);
-      precompute(root->right);
-}
-
-int compute(node *root)
-{
-      if (root == NULL)
-            return 0;
-      int d = ht[root->left] + ht[root->right] + 1;
-      int l = compute(root->left);
-      int r = compute(root->right);
-
-      return max(d, max(l, r));
-}
-
-int diameter2(node *root)
-{
-      precompute(root);
-      return compute(root);
-}
+it requires three traveral of the tree.(2 to create the path1 and path2 vectors and 1 for comparing the equal nodes of the path vectors).
 
 
-/*
-method 3: 
-remove the overhead caused by the map in the above solution.
-just modify the height function.
-*/
-int ans = 0;
-int diameter3(node*root)
-{
-      if(root == NULL) return 0;
-      int x = diameter3(root->left);
-      int y = diameter3(root->right);
-      ans = max(ans,x+y+1);
-      return max(x+1,y+1);
 
-}
-
+**********************************************************************************************************************************************************************************/
 int main()
 {
       node *root = new node(10);
@@ -100,16 +86,27 @@ int main()
       root->left->right = new node(70);
       root->left->right->right = new node(80);
       root->left->right->right->right = new node(90);
-      cout<<"pass\n";
-      int ans = diameter(root);
-      cout<<"pass\n";
-      int ans2 = diameter2(root);
-      cout<<"pass\n";
-      diameter3(root);
-      cout<<"pass\n";
-      int ans3 = ::ans;
-      cout<<ans<<"\n"<<ans2<<"\n"<<ans3<<"\n";
+
+      /*
+                                10
+                             /     \
+                           30       20
+                        /    \
+                      40      70
+                     /  \      \
+                  50     60     80
+                                 \
+                                  90
+      */
+      // lca of 60 , 90
+      node *x = LCA(root, 60, 90);
+      cout << "the LCA of 60 and 90 is: " << x->data << "\n";
+      // lca of 50 , 60
+      node *y = LCA(root, 50, 60);
+      cout << "the LCA of 50 and 60 is: " << y->data << "\n";
+      // lca of 60 , 40
+      node *z = LCA(root, 60, 40);
+      cout << "the LCA of 60 and 40 is: " << z->data << "\n";
 
       return 0;
 }
-
