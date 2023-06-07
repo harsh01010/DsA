32c97076a03e4ba5dc5625337a739d8b78ff2588
Atul <atulharsh273@gmail.com>
2023-06-07 16:58:02 +0530

    houseRobber2

--- t19_DP/z.cpp
+++ t19_DP/z.cpp
@@ -1,100 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 typedef long long int ll;
 using namespace std;
 
-// maximum sum of non adjacent elements
+// house robber 2
+/*
+we don't need new logic for this question , it can be solved using the same logic of the house robber 1
 
-// recursive
-int f(vector<int>&arr,int i)
+    find the maximum non adjacent from 0 to size -1 and 1 to size  and return the maximum of them(since we cannot take first and last simultaneously)
+*/
+
+int houseRobOne(vector<int> nums, int l, int h)
 {
-    if(i == arr.size()-1)
-        return arr[i];
-
-    int x = arr[i];
-    if(i+1<arr.size()-1)
-    x = arr[i] + f(arr,i+2);
-    int y = f(arr,i+1);
-
-    return max(x,y);
-}
-
-
-// memoization
-
-int f2(vector<int>&arr,vector<int>&dp,int i)
-{
-    if(dp[i] !=-1)
-        return dp[i];
-
-    
-    int x = arr[i] + f2(arr,dp,i+2); // since we have already defined dp[i-1] and dp[i-2] , hence we need to care about the i+2 being greater than the array size
-    int y = f2(arr,dp,i+1);
-
-    return dp[i]=max(x,y);
-
-}
-
-int f1(vector<int>arr)
-{   int i=0;
-    vector<int>dp(arr.size(),-1);
-    dp[arr.size()-1] = arr[arr.size()-1];
-    if(arr.size()>1)
-    dp[arr.size()-2] = max( arr[arr.size()-1],arr[arr.size()-2]);
-    
-    int x = f2(arr,dp,i);
-    for(auto it:dp)
-        std::cout<<it<<" ";
-    std::cout<<"\n";
-    return x;
-
-}
-
-//Tabulation
-int f3(vector<int>arr)
-{
-    vector<int>dp(arr.size(),-1);
-    dp[arr.size()-1] = arr[arr.size()-1];
-    dp[arr.size()-2] = max(arr[arr.size()-1],arr[arr.size()-2]);
-
-    if(arr.size()<3)
-        return dp[0];
-    for(int i = arr.size()-3;i>=0;i--)
+    int prev = nums[l];
+    int nprev = 0;
+    int ans = max(prev, nprev);
+    for (int i = l + 1; i <= h; i++)
     {
-        dp[i] = max(arr[i]+dp[i+2],dp[i+1]);
+        int ele = nums[i];
+        int consider = ele + nprev;
+        int notConsider = prev;
+
+        ans = max(consider, notConsider);
+        nprev = prev;
+        prev = ans;
     }
 
-    return dp[0];
+    return ans;
 }
 
-// space optimized
-int f4(vector<int>arr)
+int rob(vector<int> nums)
 {
-    int x1 = arr[arr.size()-1];
-    if(arr.size()==1)return x1;
-    int x2 = max(arr[arr.size()-1],arr[arr.size()-2]);
-    int res = max(x1,x2);
-    if(arr.size()<3) return res;
-    for(int i = arr.size()-3;i>=0;i--)
-    {
-    
-    res= max(arr[i]+x1 , x2);
-    x1 = x2;
-    x2 = res;
-    }
-    return res;
-}
+    if (nums.size() == 1)
+        return nums[0];
+    int ans1 = houseRobOne(nums, 0, nums.size() - 2);
+    int ans2 = houseRobOne(nums, 1, nums.size() - 1);
 
+    return max(ans1, ans2);
+}
 int main()
 {
-    
-    vector<int>arr = {2,1,1,2};
-    std::cout<<f(arr,0)<<"\n";
-    std::cout<<f1(arr)<<'\n';
-    std::cout<<f3(arr)<<"\n";
-    std::cout<<f4(arr)<<"\n";
 
+    vector<int>arr;
+    int n; std::cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x; std::cin>>x;
+        arr.push_back(x);
+    }
 
-
-
-return 0;
+    return 0;
 }
