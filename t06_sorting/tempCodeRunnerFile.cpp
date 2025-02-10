f923550942fe640b18860b8779d90128c392a8f8
Atul Harsh <atulharsh273@gmail.com>
2025-02-11 00:10:54 +0530

    reset

--- t06_sorting/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
- // #ifndef ONLINE_JUDGE
-  // freopen("i_p.txt", "r", stdin);
-  // freopen("o_p.txt", "w", stdout);
-  // #endif
--- t20_BinarySearch/LowerAndUpperBound.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-typedef long long int ll;
-using namespace std;
-
-int main()
-{
-
-    return 0;
-}
